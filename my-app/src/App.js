9667ee3016c30dc45110ae13cf669a142a4a6c76
KingBoolean89 <aking893.ak@gmail.com>
2019-11-22 14:31:58 -0500

    first commit, setup components

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Search from './components/Search';
+import Pokemon from './components/Pokemon';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      <Pokemon />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Pokemon() {
+  return (
+    <div className="App">
+      Pokemon
+    </div>
+  );
+}
+
+export default Pokemon;
